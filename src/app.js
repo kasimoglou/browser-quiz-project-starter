be74e200945bc0521685801eef28b720a23f94a1
Sally-samaan <samaan.sally@gmail.com>
2023-02-05 15:03:18 +0100

    new styling

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ import { initWelcomePage } from './pages/welcomePage.js';
 const loadApp = () => {
   quizData.currentQuestionIndex = 0;
   document.title = 'Singularity';
-  setFavicons('/public/images/Favicon.png');
+
   initWelcomePage();
 };
 
-function setFavicons(favImg) {
-  let headTitle = document.querySelector('head');
-  let setFavicon = document.createElement('link');
-  setFavicon.setAttribute('rel', 'shortcut icon');
-  setFavicon.setAttribute('href', favImg);
+function setFavicons(favImg){
+  const headTitle = document.querySelector('head');
+  const setFavicon = document.createElement('link');
+  setFavicon.setAttribute('rel','shortcut icon');
+  setFavicon.setAttribute('href',favImg);
   headTitle.appendChild(setFavicon);
 }
+setFavicons('/public/images/Favicon.png');
 
 window.addEventListener('load', loadApp);
